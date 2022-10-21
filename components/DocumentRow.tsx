239a96131b5f6b97b4cd9d5f6c8bdb620e68dc26
Victor Patru <patruvictor.mihai@gmail.com>
2022-10-21 13:11:27 +0300

    change: add routing to DocumentRow component

--- components/DocumentRow.tsx
+++ components/DocumentRow.tsx
@@ -9,8 +9,13 @@ interface DocumentProps {
 }
 
 function DocumentRow({ id, filename, date }: DocumentProps) {
+  const router = useRouter();
+
   return (
-    <div className="flex items-center max-w-3xl mx-auto p-5 rounded-lg hover:bg-gray-100 text-gray-700 text-base cursor-pointer">
+    <div
+      className="flex items-center max-w-3xl mx-auto p-5 rounded-lg hover:bg-gray-100 text-gray-700 text-base cursor-pointer"
+      onClick={() => router.push(`/doc/${id}`)}
+    >
       <i className="fa-solid fa-file-word fa-xl text-[#1e96f2]" aria-hidden></i>
       <p className="flex-grow pl-5 w-10 pr-10 truncate">{filename}</p>
       <p className="pr-5 text-sm">{date?.toDate().toLocaleDateString()}</p>
